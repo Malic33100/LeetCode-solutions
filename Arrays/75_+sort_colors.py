3bdbe740df6d81dfa391a160cea2818eecafa377
Malik Mertus <mertus.malik@gmail.com>
2025-10-06 16:58:25 -0400

    Solved 75, 1st LC medium

---
+++ Arrays/75_+sort_colors.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+
+
+        '''
+        The problem had a hint that stated the problem could be solved in Constant time which meant 
+        no loops and that the array had to be modified in place, so i assumed it was an in-place function
+        
+        '''
